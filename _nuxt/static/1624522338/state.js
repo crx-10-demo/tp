b9487c43e558a97cd51aff4ffc75f22f88052b13
crx-10-demo <tsuyoshi.nakayama.ka@m-tec.co.jp>
2021-06-24 17:24:10 +0900

    一旦削除

--- _nuxt/static/1624522338/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624522338",layout:"default",error:a,serverRendered:true,routePath:b,config:{STATIC_URL:"\u002Fmatris.js\u002F",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
